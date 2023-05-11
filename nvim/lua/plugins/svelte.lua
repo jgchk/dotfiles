ee2f9095832e14e8ace3b21746213390b4829c50
Jake Cheek <jake@f-m.fm>
2023-05-11 14:25:46 -0400

    use prettierd for svelte formatting

---
+++ nvim/lua/plugins/svelte.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- make sure mason installs the server
+      servers = {
+        ---@type lspconfig.options.svelte
+        svelte = {
+          settings = {
+            svelte = {
+              format = {
+                enable = false,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_)
+      local nls = require("null-ls")
+      nls.builtins.formatting.prettierd.with({
+        extra_filetypes = { "svelte" },
+      })
+    end,
+  },
+}
