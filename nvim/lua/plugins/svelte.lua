37c313b54088b12157566ed4f7f784a9415376a0
Jake Cheek <jake@f-m.fm>
2024-05-09 19:21:03 -0400

    auto refresh ts server when changing svelte files

--- nvim/lua/plugins/svelte.lua
+++ nvim/lua/plugins/svelte.lua
@@ -13,6 +13,25 @@ return {
               },
             },
           },
+
+          on_attach = function(client, bufnr)
+            if client.name == "svelte" then
+              vim.api.nvim_create_autocmd("BufWritePost", {
+                pattern = { "*.js", "*.ts", "*.svelte" },
+                callback = function(ctx)
+                  client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
+                end,
+              })
+            end
+            if vim.bo[bufnr].filetype == "svelte" then
+              vim.api.nvim_create_autocmd("BufWritePost", {
+                pattern = { "*.js", "*.ts", "*.svelte" },
+                callback = function(ctx)
+                  client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
+                end,
+              })
+            end
+          end,
         },
       },
     },
