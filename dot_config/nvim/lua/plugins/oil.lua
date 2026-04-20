ba217cf9ecd959a52f33fc24eb0b0edeb3558534
Jake Cheek <jake.cheek@itential.com>
2026-04-20 16:15:07 -0400

    fix(nvim): unbind C-h/j/k/l in oil to restore tmux-navigator
    Oil's default keymaps for these keys shadow the global vim-tmux-navigator
    bindings, preventing pane navigation from oil buffers. Setting them to false
    lets the navigator mappings fall through.

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,14 @@
 return {
   src = "https://github.com/stevearc/oil.nvim",
   setup = function()
-    require("oil").setup()
+    require("oil").setup({
+      keymaps = {
+        ["<C-h>"] = false,
+        ["<C-j>"] = false,
+        ["<C-k>"] = false,
+        ["<C-l>"] = false,
+      },
+    })
     vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
   end,
 }
