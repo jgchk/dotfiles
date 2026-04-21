8a51a8fd2aeff1f8458423b8bccfb8ea32981543
Jake Cheek <jake.cheek@itential.com>
2026-04-21 13:44:03 -0400

    feat(nvim): enable hybrid line numbers

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,5 +1,9 @@
 vim.opt.clipboard = "unnamedplus"
 
+-- Line numbers: absolute on current line, relative on others
+vim.opt.number = true
+vim.opt.relativenumber = true
+
 -- Disable netrw (must be set before plugins load)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
