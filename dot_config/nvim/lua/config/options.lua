8ffa9e0f090b6b6bf960db71acba37834236b452
Jake Cheek <jake.cheek@itential.com>
2026-04-21 13:59:39 -0400

    feat(nvim): disable statusline

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,4 @@
+-- Use system clipboard
 vim.opt.clipboard = "unnamedplus"
 
 -- Line numbers: absolute on current line, relative on others
@@ -7,3 +8,6 @@ vim.opt.relativenumber = true
 -- Disable netrw (must be set before plugins load)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+-- Disable statusline
+vim.opt.laststatus = 0
